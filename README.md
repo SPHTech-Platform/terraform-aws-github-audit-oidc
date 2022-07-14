8524c8eeff6fcd9de67574f4eb1d656b1c10ce1d
Yong Wen Chua <lawliet89@users.noreply.github.com>
2022-07-14 20:58:34 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Terraform Modules Template
+# Terraform AWS Github OIDC
+
+- [Github Actions Runner OIDC](https://docs.github.com/en/actions/deployment/security-hardening-your-deployments/configuring-openid-connect-in-amazon-web-services): Use
+  [`philips-labs/terraform-aws-github-oidc`](https://github.com/philips-labs/terraform-aws-github-oidc)
+- [Github Audit Log Streaming to S3](https://docs.github.com/en/enterprise-cloud@latest/admin/monitoring-activity-in-your-enterprise/reviewing-audit-logs-for-your-enterprise/streaming-the-audit-log-for-your-enterprise#setting-up-streaming-to-s3-with-openid-connect):
+   Use `modules/audit_log`
---
+++ modules/audit_log/main.tf
@@ -0,0 +1,40 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://oidc-configuration.audit-log.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+  thumbprint_list = [
+    "7e6db7b7584d8cf2003e0931e6cfc41a3a62d3df", # Generate with `thumbprint.sh`
+  ]
+}
+
+module "github_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "~> 5.2.0"
+
+  create_role = true
+
+  role_name        = "github-audit-logs"
+  role_description = "Used by Github to stream audit logs to S3"
+
+  role_permissions_boundary_arn = var.iam_boundary_arn
+
+  provider_url = aws_iam_openid_connect_provider.github.url
+  oidc_fully_qualified_subjects = [
+    "https://github.com/${var.github_enterprise}", # Enterprise Name
+  ]
+  oidc_fully_qualified_audiences = [
+    "sts.amazonaws.com",
+  ]
+}
+
+data "aws_iam_policy_document" "audit_bucket" {
+  statement {
+    actions = ["s3:PutObject"]
+    #tfsec:ignore:aws-iam-no-policy-wildcards
+    resources = [
+      "${var.bucket_arn}/*",
+    ]
+  }
+}
