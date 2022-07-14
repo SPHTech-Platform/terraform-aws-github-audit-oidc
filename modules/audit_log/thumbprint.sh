5e2dc15615598f680fd19b4d8cb5be13183d3514
Yong Wen Chua <lawliet89@users.noreply.github.com>
2022-07-14 21:02:03 +0800

    Fixes

--- modules/audit_log/thumbprint.sh
+++ modules/audit_log/thumbprint.sh
@@ -7,7 +7,9 @@ set -euo pipefail
 
 HOST=$(curl https://oidc-configuration.audit-log.githubusercontent.com/.well-known/openid-configuration |
 	jq -r '.jwks_uri | split("/")[2]')
-echo | openssl s_client -servername $HOST -showcerts -connect $HOST:443 2>/dev/null |
+
+# shellcheck disable=SC2016
+echo | openssl s_client -servername "${HOST}" -showcerts -connect "${HOST}:443" 2>/dev/null |
 	sed -n -e '/BEGIN/h' -e '/BEGIN/,/END/H' -e '$x' -e '$p' | tail +2 |
 	openssl x509 -fingerprint -noout |
 	sed -e "s/.*=//" -e "s/://g" |
